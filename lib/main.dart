322c423b389279d0ded5c65ff1835d36a8d10d54
elmakhtar10 <mohamedelmakhtarba@gmail.com>
2026-03-12 13:26:22 +0000

    refactor: du main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sunu_task/core/theme/app_theme.dart';
+import 'package:sunu_task/providers/auth_provider.dart';
+import 'package:sunu_task/screens/auth/login_screen.dart';
 import 'package:sunu_task/screens/splash/splash_screen.dart';
 import 'package:sunu_task/services/storage_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await StorageService.instance.init();
-
   runApp(const SunuTask());
 }
 
 class SunuTask extends StatelessWidget {
   const SunuTask({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.light,
-      home: SplashScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        // Tu ajouteras AppProvider, ProjectProvider, etc. ici plus tard
+      ],
+      child: MaterialApp(
+        title: 'SunuTask',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: ThemeMode.light,
+        home: const SplashScreen(),
+      ),
     );
   }
 }
