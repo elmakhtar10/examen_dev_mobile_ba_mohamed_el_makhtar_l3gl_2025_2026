47657b723568fd2edab001508ecfde4df0806cc7
elmakhtar10 <mohamedelmakhtarba@gmail.com>
2026-03-12 12:46:36 +0000

    feature: implementation du champ de texte personnalise

---
+++ lib/widgets/common/custom_text_field.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  final String label;
+  final TextEditingController? controller;
+  final String? hint;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final IconData? prefixIcon;
+  final int maxLines;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    this.controller,
+    this.hint,
+    this.validator,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.prefixIcon,
+    this.maxLines = 1,
+  });
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: widget.controller,
+      obscureText: widget.obscureText && !_isPasswordVisible,
+      keyboardType: widget.keyboardType,
+      validator: widget.validator,
+      maxLines: widget.maxLines,
+      decoration: InputDecoration(
+        labelText: widget.label,
+        hintText: widget.hint,
+        prefixIcon: widget.prefixIcon != null ? Icon(widget.prefixIcon) : null,
+        border: const OutlineInputBorder(),
+        suffixIcon: widget.obscureText
+            ? IconButton(
+          icon: Icon(
+            _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+          ),
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
+        )
+            : null,
+      ),
+    );
+  }
+}
