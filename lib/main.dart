81a603c0d23c6b975d798ff79968f1cf35aa6301
elmakhtar10 <mohamedelmakhtarba@gmail.com>
2026-03-12 13:35:48 +0000

    feature: ajout du package provider dans pubscpec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sunu_task/core/theme/app_theme.dart';
 import 'package:sunu_task/providers/auth_provider.dart';
-import 'package:sunu_task/screens/auth/login_screen.dart';
 import 'package:sunu_task/screens/splash/splash_screen.dart';
 import 'package:sunu_task/services/storage_service.dart';
 
